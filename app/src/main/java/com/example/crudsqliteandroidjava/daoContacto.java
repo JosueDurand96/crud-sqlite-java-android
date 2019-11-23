ca20b50ed416503359b064f56dfc6773fc34fab8
JosueDurand96 <josue.durand8@gmail.com>
2019-11-23 03:18:58 -0500

    class database

--- app/src/main/java/com/example/crudsqliteandroidjava/daoContacto.java
+++ app/src/main/java/com/example/crudsqliteandroidjava/daoContacto.java
@@ -6,13 +6,15 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 
 public class daoContacto {
-    SQLiteDatabase cx;
+    SQLiteDatabase sqLiteDatabase;
     ArrayList<Contacto> lista;
     Contacto contacto;
     Context context;
     String nombreDB = "BDContactos";
     String tabla = "create table if not exists contacto(id integer primary key autoincrement, nombre text, telefono text, email text, edad integer)";
 
+    public daoContacto(Context context){
+        this.context = context;
 
-
+    }
 }
