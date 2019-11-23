2b7c6dc1d207358a73a3dfd203cf491e2b128930
JosueDurand96 <josue.durand8@gmail.com>
2019-11-23 03:11:51 -0500

    class

--- app/src/main/java/com/example/crudsqliteandroidjava/Contacto.java
+++ app/src/main/java/com/example/crudsqliteandroidjava/Contacto.java
@@ -6,4 +6,56 @@ public class Contacto {
     String telefono;
     String email;
     int edad;
+
+    public Contacto() {
+
+    }
+
+    public Contacto(int id, String nombre, String telefono, String email, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
