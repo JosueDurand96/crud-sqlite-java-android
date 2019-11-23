0522b2ac0f2dae377407b07f15c4c7d44422d420
JosueDurand96 <josue.durand8@gmail.com>
2019-11-23 03:47:25 -0500

    main activity

--- app/src/main/java/com/example/crudsqliteandroidjava/MainActivity.java
+++ app/src/main/java/com/example/crudsqliteandroidjava/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.crudsqliteandroidjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +25,23 @@ public class MainActivity extends AppCompatActivity {
         adapter = new Adapter(this, lista, daoContacto);
 
         ListView lstContacto = (ListView) findViewById(R.id.lstContacto);
+
+        ImageView imgAgregar =(ImageView)findViewById(R.id.imgAgregar);
+
+        lstContacto.setAdapter(adapter);
+        lstContacto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        imgAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
