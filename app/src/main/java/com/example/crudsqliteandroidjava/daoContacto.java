376a19737e277c0b1057c930961df136eae30209
JosueDurand96 <josue.durand8@gmail.com>
2019-11-23 03:13:41 -0500

    daoConatctr

--- app/src/main/java/com/example/crudsqliteandroidjava/daoContacto.java
+++ app/src/main/java/com/example/crudsqliteandroidjava/daoContacto.java
@@ -1,4 +1,11 @@
 package com.example.crudsqliteandroidjava;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
 public class daoContacto {
+    SQLiteDatabase cx;
+    ArrayList<Contacto> lista;
+
 }
